e2e7e5b34e0fbdd8be3203fabc3a5a6779baba37
MohammedAlhawamdeh <hawamdeh9393@gmail.com>
2019-12-04 12:43:00 +0200

    exam

---
+++ app.js
@@ -0,0 +1,47 @@
+function greaterThan(){
+    var arr = [10,20,30,40,50];
+    var x = arr.length;
+    for(j=0;j<x;j++){
+        if (x > arr.length[i]){
+            return x;
+        }
+        if (x <= arr.length){
+            return '0';
+        }
+    }
+}
+greaterThan();
+
+
+
+// writing string that include all letters.
+function allCaps(){
+    var arr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    
+    
+}
+allCaps();
+
+
+//Stars Question
+// *
+// **
+// ***
+// ****
+// *****
+//Having   colomuns &  raws , concept would be looping through 5 colomuns and 5 rows to draw the stars.using for (i), for (j).
+
+
+
+function stars(){
+    var raw = 0;
+    var colomun = 0;
+    x ='*';
+    for(i=0,i>10,i++)
+    if (raw === colomun) {
+        return x;
+    }
+
+    
+}
+stars();
